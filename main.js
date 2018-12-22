3d4cb719fdeab7955cbde9c3c34081c7cf2807b3
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-21 18:05:30 -0600

    Toggle images when clicking

--- main.js
+++ main.js
@@ -4,12 +4,55 @@ const btnRock = document.querySelector('#btnRock');
 const btnPaper = document.querySelector('#btnPaper');
 const btnScissors = document.querySelector('#btnScissors');
 
+const imgRock = document.querySelector('#rock');
+const imgPaper = document.querySelector('#paper');
+const imgScissors = document.querySelector('#scissors');
+const imgUt = document.querySelector('#ut');
+
 let playerChoice = "";
+
+const toggleRock = () => {
+    imgRock.style.display = 'block';
+    imgPaper.style.display = 'none';
+    imgScissors.style.display = 'none';
+    imgUt.style.display = 'none';
+};
+
+const togglePaper = () => {
+    imgRock.style.display = 'none';
+    imgPaper.style.display = 'block';
+    imgScissors.style.display = 'none';
+    imgUt.style.display = 'none';
+};
+
+const toggleScissors = () => {
+    imgRock.style.display = 'none';
+    imgPaper.style.display = 'none';
+    imgScissors.style.display = 'block';
+    imgUt.style.display = 'none';
+};
+
+const toggleImages = (playerChoice) => {
+    if(playerChoice == 'rock'){
+        toggleRock();
+    }  else if(playerChoice == 'paper'){
+        togglePaper();
+    } else if(playerChoice == 'scissors'){
+        toggleScissors();
+    } else {return};
+}
+
+
 
 
 const setChoice = (e) => {
     playerChoice = e.target.getAttribute('data-choice');
+    toggleImages(playerChoice);
     log(playerChoice);
+
+    
+
+
 }
 
 
